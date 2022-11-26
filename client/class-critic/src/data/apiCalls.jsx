e2dd605fd76b2c7dd34a969645c06311e41e9e3a
Dan Lopez <dan.g.lopez@outlook.com>
2022-11-26 20:15:57 +1000

    :rocket: Deploy to netlify

--- client/class-critic/src/data/apiCalls.jsx
+++ client/class-critic/src/data/apiCalls.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { inputCleanUp } from "../functions/inputCleanup"
 
-const clientPort = process.env.CLIENT_HOST_NAME || "3000";
-const clientHost = process.env.CLIENT_PORT || "127.0.0.1";
+const clientPort = process.env.CLIENT_PORT || "3000";
+const clientHost = process.env.CLIENT_HOST_NAME || "127.0.0.1";
 const clientProtocol = process.env.CLIENT_PROTOCOL || "http";
 const URL = `${clientProtocol}://${clientHost}:${clientPort}`;
 // const PORT = "";
